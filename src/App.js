676b6773dfa45fd9ef7a3a2ce80a9fb7f0e28cb9
winda-helena <windahelena16@gmail.com>
2022-06-10 22:49:10 +0700

    todos

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TodoList from '../src/components/TodoList'
+import '../src/components/App.css'
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
